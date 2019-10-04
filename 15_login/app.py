a48d8e966c586ed6e022fa4e2fa1dc5b544dc412
William Lin <wlin01@stuy.edu>
2019-10-04 06:50:08 -0400

    finished

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,30 +1,57 @@
-# William Lin
+# William Lin, Justin Chen
 # SoftDev1 pd2
-# K #:
-#
+# K #15: Flask with login and logout
+# 2019-10-03
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, session
+import os
+
 app = Flask(__name__) #create instance of class flask
 username = "bumpkins"
 password = "bigbumpkins"
+loggedOut = False
+app.secret_key = os.urandom(32)
 
-@app.route("/") #assign following fxn to run when root route requested
+@app.route("/", methods = ["GET", "POST"]) #assign following fxn to run when root route requested
 def hello_world():
-    print(__name__)
+    global loggedOut
+    print("    USER ON HOMEPAGE")
+    if (loggedOut): # If the user is back on homepage after logging in, they logged out
+        session.pop("USER")
+        session.pop("PASS")
+        print("    USER LOGGED OUT")
+        print("    SESSION ENDED")
+        print("    " + str(session))
     return render_template("seed.html") #renders an html file to redirect to "/welcome"
 
-@app.route("/wecome")
+@app.route("/welcome", methods = ["GET", "POST"])
 def welcome():
-    print (request.cookies.get("username"))
-    user = request.cookies.get(username)
-    passwd = request.cookies.get(password)
-    if (user == username and passwd == password):
-        return ("Welcome! You have successfully logged in.")
-    return redirect("/error")
+    print("    USER LOGGED IN")
+    user = request.form["username"] #stores uername
+    passwd = request.form["password"] #stores password
+    print("    USER: " + str(user))
+    print("    PASS: " + str(passwd))
+    session["USER"] = user
+    session["PASS"] = passwd
+    print("    SESSION STARTED")
+    print("    " + str(session))
+    if (user == username and passwd == password): #checks login info
+        global loggedOut
+        loggedOut = True
+        return render_template("welcome.html")
+    return redirect(url_for("error"))
 
-@app.route("/error")
+@app.route("/error", methods = ["GET", "POST"])
 def error():
-    return "Wrong info"
+    print("    USER ON ERROR PAGE")
+    user = session["USER"]
+    passwd = session["PASS"]
+    whichError = 0; # Password is wrong
+    if (user != username and passwd != password):
+        whichError = 1 # Both are wrong
+    elif (user != username):
+        whichError = 2 # Username is wrong
+    return render_template("error.html", whichError = whichError)
 
 if __name__ == "__main__":
     app.debug = True
