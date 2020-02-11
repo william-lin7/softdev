75a32377c597efa49b1089b08bd98ee1439688b2
William Lin <wlin01@stuy.edu>
2020-02-11 09:08:02 -0500

    done

---
+++ 06_canvas/dotconn.js
@@ -0,0 +1,40 @@
+/*
+William Lin, Derek Leung
+SoftDev Pd. 2
+K #06: Dot Dot Dot
+2020-02-11
+*/
+
+var xcoor = null;
+var ycoor = null;
+
+
+var clearScreen = function(e){
+  var c = document.getElementById("playground");
+  var ctx = c.getContext("2d");
+  ctx.clearRect(0,0,c.width,c.height);
+	xcoor = null;
+	ycoor = null;
+};
+
+var button = document.getElementById('clear');
+button.addEventListener('click',clearScreen);
+
+
+var draw = function(event){
+    var c = document.getElementById("playground");
+    var ctx = c.getContext("2d");
+    ctx.beginPath(); //like pd in netlogo?
+    ctx.arc(event.offsetX, event.offsetY, 5, 0, 2 * Math.PI);
+    ctx.fill();
+		if (xcoor != null && ycoor != null){
+			ctx.lineTo(xcoor, ycoor);
+			ctx.stroke();
+		}
+		xcoor = event.offsetX;
+		ycoor = event.offsetY;
+		console.log(xcoor);
+};
+
+var canvas = document.getElementById("playground");
+canvas.addEventListener('click', draw);
